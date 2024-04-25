1e2c552821be66cbeefbf7cbd807a7c135403548
kagg886 <iveour@163.com>
2024-04-25 22:15:26 +0800

    build: 🔖 发布v3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kagg886.sylu_eoa"
         minSdk = 28
         targetSdk = 34
-        versionCode = 21
-        versionName = "3.3"
+        versionCode = 22
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
