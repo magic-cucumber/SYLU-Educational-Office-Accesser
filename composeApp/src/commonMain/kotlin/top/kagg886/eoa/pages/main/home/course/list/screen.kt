218f4023f1289522b1b7ea65d564a60862d18713
kagg886 <iveour@163.com>
2025-07-04 13:57:35 +0800

    修复了跳转课程中UI割裂的问题

--- composeApp/src/commonMain/kotlin/top/kagg886/eoa/pages/main/home/course/list/screen.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/eoa/pages/main/home/course/list/screen.kt
@@ -105,7 +105,10 @@ fun CourseListScreen() {
                                 modifier = Modifier.clickable {
                                     model.selectToWeek(it)
                                     jumpModal = false
-                                }
+                                },
+                                colors = ListItemDefaults.colors(
+                                    containerColor = BottomSheetDefaults.ContainerColor
+                                )
                             )
                         }
                     }
