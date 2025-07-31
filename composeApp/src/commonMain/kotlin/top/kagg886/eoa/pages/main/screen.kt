527ba4da24677c856b61b41164522c7259cac320
kagg886 <iveour@163.com>
2025-07-31 11:26:07 +0800

    修复了登出时不提示登出成功的bug

---
+++ composeApp/src/commonMain/kotlin/top/kagg886/eoa/pages/main/screen.kt
@@ -0,0 +1,51 @@
+package top.kagg886.eoa.pages.main
+
+import androidx.compose.runtime.Composable
+import org.orbitmvi.orbit.compose.collectSideEffect
+import top.kagg886.eoa.LocalNavController
+import top.kagg886.eoa.LocalSnackBarHost
+import top.kagg886.eoa.pages.login.LoginRoute
+import top.kagg886.eoa.util.SnackBarType
+import top.kagg886.eoa.util.SnackBarType.*
+import top.kagg886.eoa.util.showSnackBar
+
+/**
+ * ================================================
+ * Author:     886kagg
+ * Created on: 2025/7/31 11:14
+ * ================================================
+ */
+
+@Composable
+fun MainScreen(content: @Composable () -> Unit) {
+    val model = mainViewModel()
+    val nav = LocalNavController.current
+    val snack = LocalSnackBarHost.current
+    model.collectSideEffect { effect ->
+        when (effect) {
+            is MainRouteViewEffect.Toast -> {
+                snack.showSnackBar(
+                    type = effect.type,
+                    title = when (effect.type) {
+                        Success -> "成功"
+                        Warning -> "警告"
+                        SnackBarType.Error -> "错误"
+                        Info -> "信息"
+                    },
+                    description = effect.message,
+                )
+            }
+
+            is MainRouteViewEffect.NavigateToLogin -> {
+                nav.navigate(LoginRoute) {
+                    popUpTo(nav.graph.id) {
+                        inclusive = true
+                    }
+                    launchSingleTop = true
+                }
+            }
+        }
+    }
+
+    content()
+}
--- composeApp/src/commonMain/kotlin/top/kagg886/eoa/pages/main/settings/logout_confirm/screen.kt
+++ composeApp/src/commonMain/kotlin/top/kagg886/eoa/pages/main/settings/logout_confirm/screen.kt
@@ -8,52 +8,20 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.style.TextAlign
-import com.dokar.sonner.rememberToasterState
 import kotlinx.serialization.Serializable
-import org.orbitmvi.orbit.compose.collectSideEffect
 import top.kagg886.eoa.LocalNavController
+import top.kagg886.eoa.LocalSnackBarHost
 import top.kagg886.eoa.component.dialog.DialogPageScaffold
-import top.kagg886.eoa.pages.login.LoginRoute
-import top.kagg886.eoa.pages.main.MainRouteViewEffect
+import top.kagg886.eoa.pages.main.MainScreen
 import top.kagg886.eoa.pages.main.mainViewModel
-import top.kagg886.eoa.util.SnackBarType.*
-import top.kagg886.eoa.util.showSnackBar
 
 @Serializable
 data object LogoutConfirmRoute
 
 @Composable
-fun LogoutConfirmScreen() {
+fun LogoutConfirmScreen() = MainScreen {
     val model = mainViewModel()
     val nav = LocalNavController.current
-    val snack = rememberToasterState()
-    model.collectSideEffect { effect ->
-        when (effect) {
-            is MainRouteViewEffect.Toast -> {
-                snack.showSnackBar(
-                    type = effect.type,
-                    title = when (effect.type) {
-                        Success -> "成功"
-                        Warning -> "警告"
-                        Error -> "错误"
-                        Info -> "信息"
-                    },
-                    description = effect.message,
-                )
-            }
-
-            is MainRouteViewEffect.NavigateToLogin -> {
-                nav.navigate(LoginRoute) {
-                    popUpTo(nav.graph.id) {
-                        inclusive = true
-                    }
-                    launchSingleTop = true
-                }
-            }
-        }
-    }
-
-
     DialogPageScaffold(
         title = { Text("退出登录") },
         icon = { Icon(Icons.AutoMirrored.Filled.Logout, contentDescription = null) },
@@ -64,6 +32,7 @@ fun LogoutConfirmScreen() {
                 Text("确认")
             }
         },
+        snack = LocalSnackBarHost.current,
         dismissButton = {
             TextButton(
                 onClick = { nav.popBackStack() },
