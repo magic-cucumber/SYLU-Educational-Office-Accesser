32688df420907e2ec151ab29a2a03cc33a4785d9
magic-cucumber <kagg886@qq.com>
2026-05-25 16:28:27 +0800

    修复状态栏可读问题

--- widgetApp/src/androidMain/kotlin/top/kagg886/eoa/theme/Theme.android.kt
+++ widgetApp/src/androidMain/kotlin/top/kagg886/eoa/theme/Theme.android.kt
@@ -12,8 +12,8 @@ actual fun SystemAppearance(isDark: Boolean) {
     LaunchedEffect(isDark) {
         val window = (view.context as? Activity)?.window ?: return@LaunchedEffect
         WindowInsetsControllerCompat(window, window.decorView).apply {
-            isAppearanceLightStatusBars = isDark
-            isAppearanceLightNavigationBars = isDark
+            isAppearanceLightStatusBars = !isDark
+            isAppearanceLightNavigationBars = !isDark
         }
     }
 }
