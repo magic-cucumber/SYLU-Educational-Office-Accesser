e532c4a93fe66919511b709f5313ff001225ca81
magic-cucumber <kagg886@qq.com>
2026-06-01 17:30:26 +0800

    jvm侧记忆窗口打开的大小和偏移

--- composeApp-backend/src/commonMain/kotlin/top/kagg886/backend/config/AppInitializeMMKV.kt
+++ composeApp-backend/src/commonMain/kotlin/top/kagg886/backend/config/AppInitializeMMKV.kt
@@ -5,16 +5,23 @@ import top.kagg886.mkmb.MMKVMode
 import top.kagg886.mkmb.mmkvWithID
 import top.kagg886.util.boolean
 import top.kagg886.util.json
+import top.kagg886.util.jsonOrNull
 import top.kagg886.util.string
 
-object AppInitializeMMKV : MMKV by MMKV.mmkvWithID("initialize-setting", mode = MMKVMode.MULTI_PROCESS), AppInitializeMMKVType {
+object AppInitializeMMKV : MMKV by MMKV.mmkvWithID("initialize-setting", mode = MMKVMode.MULTI_PROCESS),
+    AppInitializeMMKVType {
     override var initialize: Boolean by boolean("initialize", false)
+    override var size: Pair<Int, Int> by json("size", 800 to 600)
+    override var offset: Pair<Int, Int>? by jsonOrNull("offset")
     override var announce: String by string("announce", "")
     override var link: List<Link> by json("link", listOf())
 }
 
 sealed interface AppInitializeMMKVType {
     var initialize: Boolean
+    var size: Pair<Int, Int>
+    var offset: Pair<Int, Int>?
+
     var announce: String
     var link: List<Link>
 }
--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -1,22 +1,31 @@
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.LocalWindowExceptionHandlerFactory
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowExceptionHandler
 import androidx.compose.ui.window.WindowExceptionHandlerFactory
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import coil3.ImageLoader
 import coil3.compose.setSingletonImageLoaderFactory
 import io.github.vinceglb.filekit.FileKit
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.sample
 import org.jetbrains.compose.resources.painterResource
 import sylu_eoa.composeapp.generated.resources.Res
 import sylu_eoa.composeapp.generated.resources.icon
+import top.kagg886.backend.config.AppInitializeMMKV
 import top.kagg886.eoa.App
 import top.kagg886.eoa.installCoilConfig
 import top.kagg886.util.asTaggedLogger
 import top.kagg886.util.initializeMMKV
+import kotlin.math.roundToInt
+import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
@@ -41,13 +50,47 @@ fun main() = application {
             }
         },
     ) {
+        val state = rememberWindowState(
+            width = AppInitializeMMKV.size.first.dp,
+            height = AppInitializeMMKV.size.second.dp,
+            position = when (val offset = AppInitializeMMKV.offset) {
+                null -> WindowPosition.PlatformDefault
+                else -> WindowPosition.Absolute(offset.first.dp, offset.second.dp)
+            }
+        )
+
+        LaunchedEffect(Unit) {
+            logger.i("App size: ${state.size.width} x ${state.size.height}, offset = ${state.position}")
+        }
+
+        LaunchedEffect(state) {
+            @OptIn(FlowPreview::class)
+            snapshotFlow { state.size }.distinctUntilChanged().sample(1.seconds).collect { (w,h) ->
+                logger.d("desktop window size changed: $w, $h")
+                AppInitializeMMKV.size = w.value.roundToInt() to h.value.roundToInt()
+            }
+        }
+
+        LaunchedEffect(state) {
+            @OptIn(FlowPreview::class)
+            snapshotFlow { state.position }.distinctUntilChanged().sample(1.seconds).collect { position ->
+                if (position is WindowPosition.PlatformDefault) {
+                    AppInitializeMMKV.offset = null
+                    return@collect
+                }
+                AppInitializeMMKV.offset = with(position) {
+                    logger.d("desktop window offset changed: $x, $y")
+                    x.value.roundToInt() to y.value.roundToInt()
+                }
+            }
+        }
+
         Window(
             title = "SYLU-EOA",
-            state = rememberWindowState(width = 800.dp, height = 600.dp),
+            state = state,
             icon = painterResource(Res.drawable.icon),
             onCloseRequest = ::exitApplication,
-        ) {
-            App()
-        }
+            content = { App() }
+        )
     }
 }
