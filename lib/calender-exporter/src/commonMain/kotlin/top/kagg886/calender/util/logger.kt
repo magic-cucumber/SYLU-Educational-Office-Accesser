ee4af3301731729b78393d04afcdf014c3fbb777
magic-cucumber <kagg886@qq.com>
2026-06-13 15:30:01 +0800

    删除旧模块

--- lib/calender-exporter/src/commonMain/kotlin/top/kagg886/calender/util/logger.kt
+++
@@ -1,5 +0,0 @@
-package top.kagg886.calender.util
-
-import top.kagg886.util.asTaggedLogger
-
-internal val logger = "NativeCalenderManager".asTaggedLogger
--- lib/calender-exporter/src/commonMain/kotlin/top/kagg886/calender/util/permissions.kt
+++
@@ -1,9 +0,0 @@
-package top.kagg886.calender.util
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import top.kagg886.calender.NativeCalenderManager
-import top.kagg886.calender.data.CalenderPermissionGrantType
-
-@Composable
-internal expect fun rememberCalenderPermissionRequester(manager: NativeCalenderManager): State<CalenderPermissionGrantType>
