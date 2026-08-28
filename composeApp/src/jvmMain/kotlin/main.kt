f2ffff5576de1228151e8918cf43459e353409aa
magic-cucumber <kagg886@qq.com>
2026-08-28 16:06:08 +0800

    restore

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -38,79 +38,79 @@ fun main() {
 
     FileKit.init(appId = "SYLU-EOA")
 
-    var lastException by mutableStateOf<Throwable?>(Throwable())
+    var lastException by mutableStateOf<Throwable?>(null)
 
-//    application(exitProcessOnExit = false) {
-//        LaunchedEffect(Unit) {
-//            Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
-//                lastException = throwable
-//                exitApplication()
-//            }
-//        }
-//
-//        setSingletonImageLoaderFactory { context ->
-//            ImageLoader.Builder(context)
-//                .installCoilConfig()
-//                .build()
-//        }
-//        CompositionLocalProvider(
-//            LocalWindowExceptionHandlerFactory provides WindowExceptionHandlerFactory {
-//                WindowExceptionHandler { throwable ->
-//                    lastException = throwable
-//                    exitApplication()
-//                }
-//            },
-//        ) {
-//            val state = rememberWindowState(
-//                width = AppInitializeMMKV.size.first.dp,
-//                height = AppInitializeMMKV.size.second.dp,
-//                position = when (val offset = AppInitializeMMKV.offset) {
-//                    null -> WindowPosition.PlatformDefault
-//                    else -> WindowPosition.Absolute(offset.first.dp, offset.second.dp)
-//                }
-//            )
-//
-//            LaunchedEffect(Unit) {
-//                logger.i("App size: ${state.size.width} x ${state.size.height}, offset = ${state.position}")
-//            }
-//
-//            LaunchedEffect(state) {
-//                @OptIn(FlowPreview::class)
-//                snapshotFlow { state.size }.distinctUntilChanged().sample(1.seconds)
-//                    .collect { (w, h) ->
-//                        logger.d("desktop window size changed: $w, $h")
-//                        AppInitializeMMKV.size = w.value.roundToInt() to h.value.roundToInt()
-//                    }
-//            }
-//
-//            LaunchedEffect(state) {
-//                @OptIn(FlowPreview::class)
-//                snapshotFlow { state.position }.distinctUntilChanged().sample(1.seconds)
-//                    .collect { position ->
-//                        if (position is WindowPosition.PlatformDefault) {
-//                            AppInitializeMMKV.offset = null
-//                            return@collect
-//                        }
-//                        AppInitializeMMKV.offset = with(position) {
-//                            logger.d("desktop window offset changed: $x, $y")
-//                            x.value.roundToInt() to y.value.roundToInt()
-//                        }
-//                    }
-//            }
-//
-//            Window(
-//                title = "SYLU-EOA",
-//                state = state,
-//                icon = painterResource(Res.drawable.icon),
-//                onCloseRequest = ::exitApplication,
-//                content = {
-//                    CompositionLocalProvider(LocalDatabase provides database) {
-//                        App()
-//                    }
-//                }
-//            )
-//        }
-//    }
+    application(exitProcessOnExit = false) {
+        LaunchedEffect(Unit) {
+            Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+                lastException = throwable
+                exitApplication()
+            }
+        }
+
+        setSingletonImageLoaderFactory { context ->
+            ImageLoader.Builder(context)
+                .installCoilConfig()
+                .build()
+        }
+        CompositionLocalProvider(
+            LocalWindowExceptionHandlerFactory provides WindowExceptionHandlerFactory {
+                WindowExceptionHandler { throwable ->
+                    lastException = throwable
+                    exitApplication()
+                }
+            },
+        ) {
+            val state = rememberWindowState(
+                width = AppInitializeMMKV.size.first.dp,
+                height = AppInitializeMMKV.size.second.dp,
+                position = when (val offset = AppInitializeMMKV.offset) {
+                    null -> WindowPosition.PlatformDefault
+                    else -> WindowPosition.Absolute(offset.first.dp, offset.second.dp)
+                }
+            )
+
+            LaunchedEffect(Unit) {
+                logger.i("App size: ${state.size.width} x ${state.size.height}, offset = ${state.position}")
+            }
+
+            LaunchedEffect(state) {
+                @OptIn(FlowPreview::class)
+                snapshotFlow { state.size }.distinctUntilChanged().sample(1.seconds)
+                    .collect { (w, h) ->
+                        logger.d("desktop window size changed: $w, $h")
+                        AppInitializeMMKV.size = w.value.roundToInt() to h.value.roundToInt()
+                    }
+            }
+
+            LaunchedEffect(state) {
+                @OptIn(FlowPreview::class)
+                snapshotFlow { state.position }.distinctUntilChanged().sample(1.seconds)
+                    .collect { position ->
+                        if (position is WindowPosition.PlatformDefault) {
+                            AppInitializeMMKV.offset = null
+                            return@collect
+                        }
+                        AppInitializeMMKV.offset = with(position) {
+                            logger.d("desktop window offset changed: $x, $y")
+                            x.value.roundToInt() to y.value.roundToInt()
+                        }
+                    }
+            }
+
+            Window(
+                title = "SYLU-EOA",
+                state = state,
+                icon = painterResource(Res.drawable.icon),
+                onCloseRequest = ::exitApplication,
+                content = {
+                    CompositionLocalProvider(LocalDatabase provides database) {
+                        App()
+                    }
+                }
+            )
+        }
+    }
 
     if (lastException != null) {
         logger.e(lastException) { "App exit with exception" }
